bb901c7fc32801074f12bb25ee7f56564e9defc7
Gary Ditsch <garyditsch@Garys-MBP.localdomain>
2021-07-13 16:31:09 -0400

    setting up bars, no svg

---
+++ horizontal_bar.js
@@ -0,0 +1,17 @@
+console.log(d3)
+
+const data = [
+    {date: 7/1/2021, value: 10},
+    {date: 7/2/2021, value: 8},
+    {date: 7/3/2021, value: 6},
+    {date: 7/4/2021, value: 14},
+    {date: 7/6/2021, value: 16}
+]
+
+d3.select("#bar_chart")
+    .selectAll("div")
+    .data(data)
+        .enter()
+        .append("div")
+        .style("width", d => d.value * 10 + "px")
+        .text(d => d.value);
