89df331edf5cf4f90a0046bf4c2e3987fad7257b
Gary Ditsch <garyditsch@Garys-MBP.localdomain>
2021-07-16 16:13:13 -0400

    updated div to svg and xScale

--- horizontal_bar.js
+++ horizontal_bar.js
@@ -5,17 +5,17 @@
 console.log(d3)
 
 const data = [
-    {date: 7/1/2021, value: 10},
-    {date: 7/2/2021, value: 8},
-    {date: 7/3/2021, value: 6},
-    {date: 7/4/2021, value: 14},
-    {date: 7/6/2021, value: 16},
-    {date: 7/7/2021, value: 16}
+    {date: '7/1/2021', value: 10},
+    {date: '7/2/2021', value: 8},
+    {date: '7/3/2021', value: 6},
+    {date: '7/4/2021', value: 14},
+    {date: '7/6/2021', value: 16},
+    {date: '7/7/2021', value: 16}
 ]
 
 //  the size of the overall svg element
 const height = 300;
-const width = 720;
+const width = 500;
 
 //  the width of each bar and the offset between each bar
 const barWidth = 40;
@@ -26,13 +26,14 @@ const yScale = d3.scaleLinear()
     .range([0,height])
 
 const xScale = d3.scaleBand()
-    .domain(d3.range(0, data.length))
+    .domain(data.map(d => d.date))
     .range([0, width])
 
-console.log((data, i) => xScale(i))
+console.log(data)
 
+let chart = d3.select('svg')
 
-d3.select('#bar_chart').append('svg')
+chart
   .attr('width', width)
   .attr('height', height)
   .style('background', '#dff0d8')
@@ -41,9 +42,18 @@ d3.select('#bar_chart').append('svg')
     .enter().append('rect')
         .style('fill', '#fff')
         .style('stroke', '#000')
-        .style('stroke-width', '2')
-        .attr('width', xScale())
+        .style('stroke-width', '1')
+        .attr('width', d => xScale.bandwidth())
         .attr('height', (data) => yScale(data.value))
-        .attr('x', (data, i) => xScale(i))
+        .attr('x', data => xScale(data.date))
         .attr('y', (data) => { return height - yScale(data.value)})
         .text(d => d.value)
+
+chart.append('g')
+    .attr('transform', `translate(0, ${height})`)
+    .call(d3.axisBottom(xScale))
+    .selectAll('text')
+        .style('text-anchor', 'end')
+        .style('font-size', '18px')
+        .style('color', '#000')
+        .attr('transform', 'rotate(-45) translate(-10, -5)')
